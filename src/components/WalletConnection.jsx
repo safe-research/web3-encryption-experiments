f1689f9b088044f237dffb6db63f7a50e04d3fa1
Nicholas Rodrigues Lordello <nick@safe.global>
2025-07-11 16:25:59 +0200

    final version

--- src/components/WalletConnection.jsx
+++ src/components/WalletConnection.jsx
@@ -30,56 +30,16 @@ function useProvidersStore() {
 
     window.addEventListener("eip6963:announceProvider", (e) => {
       const { info, provider } = e.detail;
-      providersStore.value = [...providersStore.value, {
-        id: info.uuid,
-        name: info.name,
-        provider,
-      }];
+      providersStore.value = [
+        ...providersStore.value,
+        {
+          id: info.uuid,
+          name: info.name,
+          provider,
+        },
+      ];
     });
     window.dispatchEvent(new Event("eip6963:requestProvider"));
-
-    // TEST +begin
-    setTimeout(
-      () =>
-        window.dispatchEvent(
-          new CustomEvent("eip6963:announceProvider", {
-            detail: {
-              info: { uuid: "default", name: "Default Provider" },
-              provider: {
-                request: async ({ method }) => {
-                  if (method === "eth_requestAccounts") {
-                    await new Promise((resolve) => setTimeout(resolve, 500));
-                    return ["0x1234567890abcdef1234567890abcdef12345678"];
-                  }
-                  throw new Error("Method not supported");
-                },
-              },
-            },
-          }),
-        ),
-      1000,
-    );
-    setTimeout(
-      () =>
-        window.dispatchEvent(
-          new CustomEvent("eip6963:announceProvider", {
-            detail: {
-              info: { uuid: "test", name: "Test Provider" },
-              provider: {
-                request: async ({ method }) => {
-                  if (method === "eth_requestAccounts") {
-                    await new Promise((resolve) => setTimeout(resolve, 500));
-                    return ["0xabcdef1234567890abcdef1234567890abcdef12"];
-                  }
-                  throw new Error("Method not supported");
-                },
-              },
-            },
-          }),
-        ),
-      1200,
-    );
-    // TEST +end
   }
 
   return useSyncExternalStore(
@@ -95,25 +55,36 @@ export function WalletConnection({ setWallet }) {
   const [isPending, startTransition] = useTransition();
 
   const selection = useMemo(() => {
-    return providers.find((p) =>
-      selectedProvider === undefined || p.id === selectedProvider
+    return providers.find(
+      (p) => selectedProvider === undefined || p.id === selectedProvider,
     );
   }, [providers, selectedProvider]);
 
-  const handleClick = useCallback((e) => {
-    if (selection) {
-      startTransition(async () => {
-        const accounts = await selection.provider.request({
-          method: "eth_requestAccounts",
+  const handleClick = useCallback(
+    (e) => {
+      if (selection) {
+        startTransition(async () => {
+          const [address] = await selection.provider.request({
+            method: "eth_requestAccounts",
+          });
+          if (address) {
+            setWallet({ ...selection, address });
+          }
         });
-        setWallet({ ...selection, accounts });
-      });
-    }
-  }, [selection, startTransition]);
+      }
+    },
+    [selection, startTransition],
+  );
 
   return (
     <>
-      <h3>Connect your wallet...</h3>
+      {wallet === null ? (
+        <h3>Connect your wallet...</h3>
+      ) : (
+        <h3>
+          Connected with <code>{wallet.address}</code>.
+        </h3>
+      )}
       <select
         value={selectedProvider}
         onChange={(e) => setSelectedProvider(e.target.value)}
@@ -125,22 +96,13 @@ export function WalletConnection({ setWallet }) {
           </option>
         ))}
       </select>
-      {wallet === null
-        ? (
-          <button
-            onClick={handleClick}
-            disabled={!selection || isPending}
-          >
-            {isPending ? "Connecting..." : "Connect"}
-          </button>
-        )
-        : (
-          <button
-            onClick={() => setWallet(null)}
-          >
-            Disconnect
-          </button>
-        )}
+      {wallet === null ? (
+        <button onClick={handleClick} disabled={!selection || isPending}>
+          {isPending ? "Connecting..." : "Connect"}
+        </button>
+      ) : (
+        <button onClick={() => setWallet(null)}>Disconnect</button>
+      )}
     </>
   );
 }
